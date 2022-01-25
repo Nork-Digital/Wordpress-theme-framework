11fd37652554d8e3bc5ea306bf73cd3ef2fb4ae5
josetropalde <josehenrique.tropalde@gmail.com>
2022-01-25 15:02:17 -0300

    update footer/header slick

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <?php wp_footer(); ?>
 
+<script type="text/javascript" src="<?php echo esc_url(get_stylesheet_directory_uri() . '/js/script.js'); ?>"></script>
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -25,7 +25,10 @@
   <?php endif; ?>
   <?php wp_head(); ?>
 
+  <!--Slick css-->
+  <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css" integrity="sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
   <!--css fontawesome-->
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/all.min.css">
