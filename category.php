5bda2d648196da9fdaaf85f263eaff2197ab3518
josetropalde <josehenrique.tropalde@gmail.com>
2022-03-22 09:36:34 -0300

    update

--- category.php
+++ category.php
@@ -2,11 +2,9 @@
 
 	<main role="main">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <a href="<?php?>">
-        <h1><?php the_title(); ?></h1>
-        <p><?php the_content(); ?></p>
-      </a>
+      <h1><?php the_title(); ?></h1>
+      <p><?php the_content(); ?></p>
     <?php endwhile; endif ?>
-	</main>
+  </main>
 
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,18 +1,17 @@
-
 <?php get_header(); ?>
 
-<main role="main">
+  <main role="main">
 
-  <?php while ( have_posts() ) : ?>
-    <?php the_post(); ?>
-      <h1><?php the_title(); ?></h1>
-      <p><?php the_content(); ?></p>
+    <?php while ( have_posts() ) : ?>
+      <?php the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <p><?php the_content(); ?></p>
 
-    <?php if ( comments_open() || get_comments_number() ) : comments_template() ?> 
+      <?php if ( comments_open() || get_comments_number() ) : comments_template() ?> 
     
-    <?php endif; ?>
-  <?php endwhile; ?>
+      <?php endif; ?>
+    <?php endwhile; ?>
 
-</main>
+  </main>
 
 <?php get_footer(); ?>
