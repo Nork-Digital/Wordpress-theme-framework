48c27edde4d389e806ea38972d30226b6c2f8000
josetropalde <josehenrique.tropalde@gmail.com>
2022-04-27 16:50:31 -0300

    updates

--- footer.php
+++ footer.php
@@ -1,6 +1,25 @@
 <?php wp_footer(); ?>
 
-<script type="text/javascript" src="<?php echo esc_url(get_stylesheet_directory_uri() . '/js/script.js'); ?>"></script>
+  <!--Glider-->
+  <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.6.6/glider.min.js"
+    integrity="sha512-RidPlemZ+Xtdq62dXb81kYFycgFQJ71CKg+YbKw+deBWB0TLIqCraOn6k0CWDH2rGvE1a8ruqMB+4E4OLVJ7Dg=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  ></script>
+
+  <!--Alertify-->
+  <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+
+  <!--FontAwesome-->
+  <script
+    src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/js/all.min.js"
+    integrity="sha512-cyAbuGborsD25bhT/uz++wPqrh5cqPh1ULJz4NSpN9ktWcA6Hnh9g+CWKeNx2R0fgQt+ybRXdabSBgYXkQTTmA=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  ></script>
+
+<script type="text/javascript" src="<?php echo esc_url(get_stylesheet_directory_uri() . 'assets/js/script.js'); ?>"></script>
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -26,12 +26,21 @@
   <?php endif; ?>
   <?php wp_head(); ?>
 
-  <!--Slick css-->
-  <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+   <!--Alertify css-->
+   <link
+      rel="stylesheet"
+      href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"
+    />
+    <!--Glider css-->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/glider-js/1.6.6/glider.min.css"
+      integrity="sha512-YM6sLXVMZqkCspZoZeIPGXrhD9wxlxEF7MzniuvegURqrTGV2xTfqq1v9FJnczH+5OGFl5V78RgHZGaK34ylVg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css" integrity="sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>assets/css/style.css">
 
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -39,32 +48,4 @@
 </head>
 
 <body <?php body_class(); ?>>
-  <header class="secao-header">
-    <div class="cont-header">
-      <div class="logo-header"> <a href="/">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/Group 705.png" alt="Logo Marco Glória" /></a>
-      </div>
-      <div class="logo-header-resp">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/Group 764.png" alt="Logo Marco Glória" />
-      </div>
-      <nav class="nav-menu-header">
-        <ul>
-          <li onclick="addClassAtivo('home');" id="home" class="nav-header-ativo"><a href="<?php echo get_page_link(15) . '#Hero' ?>">Home</a></li>
-          <li onclick="addClassAtivo('sobre');" id="sobre"><a href="<?php echo get_page_link(15) . '#Sobre' ?>">Sobre</a></li>
-          <li onclick="addClassAtivo('servicos');" id="servicos"><a href="<?php echo get_page_link(15) . '#Serviços' ?>">Serviços</a></li>
-          <li onclick="addClassAtivo('projetos');" id="projetos"><a href="<?php echo get_page_link(15) . '#Projetos' ?>">Projetos</a></li>
-          <li onclick="addClassAtivo('contato');" id="contato"><a href="<?php echo get_page_link(15) . '#Contato' ?>">Contato</a></li>
-        </ul>
-      </nav>
-      <div class="btns-header">
-        <a href="#Contato" class="btn-mg btn-header">Orçamento</a>
-      </div>
-      <div class="redes-header">
-        <a href="<? echo $fb_redes_mg ?>" target="_blank"><i class="fab fa-facebook-f"></i></a>
-        <a href="<? echo $insta_redes_mg ?>" target="_blank"><i class="fab fa-instagram"></i></a>
-      </div>
-      <div class="menu-hamburger">
-        <span></span>
-      </div>
-    </div>
-  </header>
+  
